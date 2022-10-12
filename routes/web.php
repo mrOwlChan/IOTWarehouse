877e5798b11ac795ce5458d14f2a893b7a1d054a
mrOwlChan <mr.scandiacus@gmail.com>
2022-10-13 01:29:44 +0700

    Instalasi Framework CSS Bootstrap 4.6 AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('templates.mainSideNavbar');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
